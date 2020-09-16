c5d90d0eeba8f0e26cfda0c24b75708a40fdd30a
lahooper <lukehooper2@gmail.com>
2020-09-16 11:22:26 +0100

    Updates

---
+++ precache-manifest.6f3fdce2553cae1ed97915711d81aeb2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b7ee5475ec68f1c95028d5f139ccc56",
+    "url": "/Yelp_clone/index.html"
+  },
+  {
+    "revision": "18f6248856043ccfbc76",
+    "url": "/Yelp_clone/static/css/main.8d191c08.chunk.css"
+  },
+  {
+    "revision": "10126e3e925687b0b41e",
+    "url": "/Yelp_clone/static/js/2.b152fa3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Yelp_clone/static/js/2.b152fa3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18f6248856043ccfbc76",
+    "url": "/Yelp_clone/static/js/main.40007b32.chunk.js"
+  },
+  {
+    "revision": "d5f799e81fd966b6b91f",
+    "url": "/Yelp_clone/static/js/runtime-main.f179067f.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/Yelp_clone/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/Yelp_clone/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Yelp_clone/static/media/logo.5d5d9eef.svg"
+  }
+]);
